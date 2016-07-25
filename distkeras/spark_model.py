22421e25e50a470ab40a3a9d7a079607e8519fe1
Joeri Hermans <joeri@joerihermans.com>
2016-07-25 16:00:35 +0200

    Add testing

--- distkeras/spark_model.py
+++ distkeras/spark_model.py
@@ -4,7 +4,11 @@ on Apache Spark.
 
 ## BEGIN Imports. ##############################################################
 
+import numpy as np
+from six.moves.cPickle as pickle
+from flask import Flask, request
 from distkeras.networking import *
+from multiprocessing import Process, Lock
 
 ## END Imports. ################################################################
 
@@ -15,3 +19,21 @@ class SparkModel:
         self.master_model = keras_model
         self.master_address = determine_host_address()
         self.master_port = master_port
+        self.mutex = Lock()
+
+    def service(self):
+        app = Flask(__name__)
+        self.app = all
+
+    @app.route('/parameters', methods=['GET'])
+    def route_parameters():
+        with self.mutex:
+            pickled_weights = picle.dumps(self.master_model.get_weights())
+
+    def start_server(self):
+        self.server = Process(target=self.service)
+        self.server.start()
+
+    def stop_server(self):
+        self.server.terminate()
+        self.server.join()
