dac02f8408b080babd24428be9b3c23ed958199c
Joeri Hermans <joeri@joerihermans.com>
2016-07-25 13:34:42 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+""" Setup-module for DistKeras.
+
+This software enables distributed Deep Learning on Apache Spark.
+
+See:
+https://github.com/JoeriHermans/DistKeras/
+http://joerihermans.com/
+"""
+
+from setuptools import setup
+from setuptoold import find_packages
+
+setup(name='dist_keras',
+      version='0.1',
+      description='Deep learning on Apache Spark with Keras.',
+      url='https://github.com/JoeriHermans/DistKeras',
+      author='Joeri Hermans',
+      author_email='joeri@joerihermans.com',
+      license='MIT',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+          # Project is intended for:
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Analytics',
+          # Python versions
+          'Programming Language :: Python :: 2.7',
+      ],
+      # Keywords related to the project.
+      keyword='deep machine learning keras apache spark distributed',
+      # Package requirements.
+      install_requires=['DistKeras'],
+      dependency_links=['git+ssh://git@github.com/JoeriHermans/DistKeras.git']
+)
