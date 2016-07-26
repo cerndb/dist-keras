dd8654d138e6d31e388ec677b82bc62348ec56cf
Joeri Hermans <joeri@joerihermans.com>
2016-07-26 14:59:04 +0200

    Update example imports

--- examples/test.py
+++ examples/test.py
@@ -1,5 +1,10 @@
-from distkeras import spark_model
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.utils import np_utils
 
-model = SparkModel(None, None)
-print(model.master_address)
-print(model.master_port)
+from distkeras.distributed_models import *
+from distkeras.optimizers import *
+
+from pyspark import SparkContext, SparkConf
