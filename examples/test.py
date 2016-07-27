b7b39ac9b570a5701c8898bebc455643c0bdbeba
Joeri Hermans <joeri@joerihermans.com>
2016-07-27 17:00:24 +0200

    Update example

--- examples/test.py
+++ examples/test.py
@@ -9,40 +9,45 @@ from distkeras import optimizers as distkeras_optimizers
 
 from pyspark import SparkContext, SparkConf
 
-batch_size = 64
+batch_size = 128
 nb_classes = 10
-nb_epoch = 10000
+nb_epoch = 20
 
 conf = SparkConf().setAppName("Dist-Keras Testing").setMaster('local[*]')
 sc = SparkContext(conf=conf)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype("float32")
-x_test = x_test.astype("float32")
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
 
-# Convert class vectors to binary class matrices
-y_train = np_utils.to_categorical(y_train, nb_classes)
-y_test = np_utils.to_categorical(y_test, nb_classes)
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
-model.add(Dense(128, input_dim=784))
+model.add(Dense(512, input_shape=(784,)))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))
-model.add(Dense(128))
+model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))
 model.add(Dense(10))
 model.add(Activation('softmax'))
-sgd = SGD(lr=0.1)
+
+model.summary()
+
+sgd = SGD()
 loss='categorical_crossentropy'
 model.compile(loss=loss, optimizer=sgd, metrics=['accuracy'])
+
 rdd = to_simple_rdd(sc, x_train, y_train)
 sgd = distkeras_optimizers.SGD()
 sparkModel = SparkModel(sc, rdd, keras_model=model, optimizer=sgd, loss=loss)
