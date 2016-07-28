fa5520047db6b747c90685dc18fdfa5e5f4b8980
Joeri Hermans <joeri@joerihermans.com>
2016-07-28 15:10:00 +0200

    Subtract the deltas

--- distkeras/distributed_models.py
+++ distkeras/distributed_models.py
@@ -92,7 +92,7 @@ class DistributedModel(object):
             delta = pickle.loads(request.data)
             weights = self.master_model.get_weights()
             original_weights = np.copy(weights)
-            weights = np.add(weights, delta)
+            weights = np.subtract(weights, delta)
             with self.mutex:
                 self.master_model.set_weights(weights)
 
