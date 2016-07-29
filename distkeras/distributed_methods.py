75d3a22ad182294e7d2f89d590897814ed8ef447
Joeri Hermans <joeri@joerihermans.com>
2016-07-29 17:00:53 +0200

    Refactor for multiple distributed methods

--- distkeras/distributed_methods.py
+++ distkeras/distributed_methods.py
@@ -25,19 +25,7 @@ def rest_send_to_master(url, data):
 
 ## END Utility functions. ######################################################
 
-class DistributedMethod(object):
-
-    def __init__(self, master_method, slave_method):
-        self.master_method = master_method
-        self.slave_method = slave_method
-
-    def get_master_model():
-        return self.master_method.master_model
-
 class MasterMethod(object):
-
-    def __init__(self, model):
-        self.master_model = model
 
     def run():
         raise NotImplementedError
@@ -49,8 +37,7 @@ class SlaveMethod(object):
 
 class NetworkMasterMethod(MasterMethod):
 
-    def __init__(self, model, network_port):
-        super(MasterMethod, self).__init__(model)
+    def __init__(self, network_port):
         self.network_port = port
 
     def run():
@@ -64,29 +51,3 @@ class NetworkSlaveMethod(SlaveMethod):
 
     def get_master_url(self):
         return self.master_address + ":" + `self.master_port`
-
-## BEGIN Implementations. ######################################################
-
-class NaiveSGDMasterMethod(NetworkMasterMethod):
-
-    def __init__(self, port, model):
-        super(NetworkMasterMethod, self).__init__(model, port)
-        self.mutex_master_model = Lock()
-        self.server = None
-
-    def run(self):
-        self.server = Process(target=self.service)
-        self.server.start()
-
-    def service(self):
-        raise NotImplementedError
-
-class NaiveSGDSlaveMethod(SlaveMethod):
-
-    def __init__(self, master_address, master_port):
-        super(SlaveMethod, self).__init__(master_address, master_port)
-
-    def run(self):
-        raise NotImplementedError
-
-## END Implementations . #######################################################
