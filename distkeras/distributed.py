9b13d718b1c755639eeb6c8045ef0d8edaa76a00
Joeri Hermans <joeri@joerihermans.com>
2016-09-06 23:53:27 +0200

    Add debugging information

--- distkeras/distributed.py
+++ distkeras/distributed.py
@@ -104,7 +104,8 @@ class ModelPredictor(Predictor):
                 X = np.asarray([row[self.features_column]])
                 Y = model.predict(X)
                 v = DenseVector(Y.tolist())
-                # new_row = new_dataframe_row(row, self.output_column, v)
+                new_row = new_dataframe_row(row, self.output_column, v)
+                print(new_row)
                 rows.append(row)
         except ValueError:
             pass
