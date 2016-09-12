072ff0339e55c5000cfeae6c84cb4075f86f7f58
Joeri Hermans <joeri@joerihermans.com>
2016-09-12 23:52:24 +0200

    Copy temporary vector

--- distkeras/distributed.py
+++ distkeras/distributed.py
@@ -295,6 +295,7 @@ class EASGD(Trainer):
     def process_variables(self):
         print("\n\n\n--- Processing Variables in iteration " + `self.iteration` + "---\n\n\n")
         center_variable = np.asarray(self.model.get_weights())
+        temp = np.copy(center_variable)
         # Iterate through all worker variables.
         for i in range(0, self.num_workers):
             temp += self.rho * (self.variables[i] - center_variable)
