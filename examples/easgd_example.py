9abfd12c56ca9717ba09291daf5a7331edd9e8fc
Joeri Hermans <joeri@joerihermans.com>
2016-09-28 22:27:23 +0200

    Modify example

--- examples/easgd_example.py
+++ examples/easgd_example.py
@@ -73,7 +73,7 @@ model.add(Activation('softmax'))
 model.summary()
 
 # Sample the dataset.
-# dataset = dataset.sample(True, 0.2, 1234)
+dataset = dataset.sample(True, 0.01, 1234)
 
 # Transform the indexed label to an vector.
 labelVectorTransformer = LabelVectorTransformer(output_dim=nb_classes, input_col="label_index", output_col="label")
@@ -83,8 +83,6 @@ dataset.printSchema()
 # Split the data in a training and test set.
 (trainingSet, testSet) = dataset.randomSplit([0.9, 0.1])
 
-trainingSet.sample(False, 0.01)
-trainingSet.cache()
 # Create the distributed Ensemble trainer.
 trainer = EASGD(keras_model=model, features_col="features_normalized", batch_size=700,
                 num_workers=num_workers, rho=5.0, learning_rate=0.05)
