775fd1be96a70f68d8679d936cb90369ad8dc92f
Joeri Hermans <joeri@joerihermans.com>
2016-10-03 11:19:07 +0200

    Add debugging information

--- distkeras/workers.py
+++ distkeras/workers.py
@@ -124,6 +124,7 @@ class AsynchronousEASGDWorker(object):
                 X = np.asarray([x[self.features_column] for x in feature_iterator])
                 Y = np.asarray([x[self.label_column] for x in label_iterator])
                 if self.iteration % self.communication_period == 0:
+                    print("Updating")
                     self.fetch_center_variable()
                     W = np.asarray(model.get_weights())
                     # Compute the elastic difference.
