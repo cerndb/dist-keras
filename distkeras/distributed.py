6c268632ed536d89852549b36f5fd7e1d48e9a48
Joeri Hermans <joeri@joerihermans.com>
2016-10-13 17:41:24 +0200

    Set default EASGD batch_size to Keras defaults

--- distkeras/distributed.py
+++ distkeras/distributed.py
@@ -424,7 +424,7 @@ class SynchronizedDistributedTrainer(Trainer):
 class EASGD(SynchronizedDistributedTrainer):
 
     def __init__(self, keras_model, worker_optimizer, loss, features_col="features", label_col="label", num_workers=2,
-                 rho=5.0, learning_rate=0.01, batch_size=1000, master_port=5000, num_epoch=1, communication_period=500):
+                 rho=5.0, learning_rate=0.01, batch_size=32, master_port=5000, num_epoch=1, communication_period=500):
         super(EASGD, self).__init__(keras_model=keras_model, num_workers=num_workers,
                                     batch_size=batch_size, features_col=features_col,
                                     label_col=label_col, worker_optimizer=worker_optimizer,
