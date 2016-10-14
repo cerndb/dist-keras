f8f210b2f0a23919df6063ed9849f68f82e1d75d
Joeri Hermans <joeri@joerihermans.com>
2016-10-14 10:01:32 +0200

    Modify

--- distkeras/workers.py
+++ distkeras/workers.py
@@ -70,15 +70,11 @@ class EASGDWorker(object):
         for i in range(0, self.num_epoch):
             batch_index = 0
             while batch_index < num_batches:
+                batch_X = batches_X[batch_index]
+                batch_Y = batches_Y[batch_index]
+                batch_index += 1
                 W1 = np.asarray(model.get_weights())
-                # Train the model during the communication period.
-                for p_i in range(0, self.communication_period):
-                    batch_X = batches_X[batch_index]
-                    batch_Y = batches_Y[batch_index]
-                    model.train_on_batch(batch_X, batch_Y)
-                    batch_index += 1
-                    if batch_index == num_batches:
-                        break
+                model.train_on_batch(batch_X, batch_Y)
                 W2 = np.asarray(model.get_weights())
                 gradient = W2 - W1
                 self.master_send_variable(index, W2)
