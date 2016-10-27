e90e6ace042b50a004e9b0cf1bf8de138f2b90c8
Joeri Hermans <joeri@joerihermans.com>
2016-10-27 11:28:45 +0200

    Modify list

--- distkeras/utils.py
+++ distkeras/utils.py
@@ -22,7 +22,7 @@ def to_dense_vector(value, n_dim=2):
     vector = np.zeros(n_dim)
     vector[value] = 1.0
 
-    return DenseVector(vector.tolist())
+    return DenseVector(vector)
 
 def new_dataframe_row_fast(old_row, column_name, column_value):
     r = Row(old_row.__fields__ + [column_name])(old_row + (column_value, ))
