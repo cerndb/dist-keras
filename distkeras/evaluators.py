a0d8707f54339121b023a8e181342f8073ebb731
Joeri Hermans <joeri@joerihermans.com>
2016-11-14 13:39:13 +0100

    Fix AccuracyEvaluator

--- distkeras/evaluators.py
+++ distkeras/evaluators.py
@@ -31,19 +31,11 @@ class AccuracyEvaluator(Evaluator):
         # Initialize the parent structure.
         super(AccuracyEvaluator, self).__init__(label_col, prediction_col)
 
-    def _evaluate(self, row):
-        """Filters the correct predictions."""
-        prediction = row[self.prediction_column]
-        label = row[self.label_column]
-        # Check if the prediction is equal to the label.
-        if label == prediction:
-            yield row
-
     def evaluate(self, dataframe):
         # Count the total number of instances.
         num_instances = dataframe.count()
-        # Evaluate all rows.
-        df = dataframe.rdd.map(self._evaluate)
+        # Extract the matching indexes.
+        df = dataframe.where(dataframe[self.prediction_col] == dataframe[self.label_col])
         # Fetch the number of correctly guessed instances.
         validated_instances = df.count()
 
