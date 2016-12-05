1c22b95dabeb8a176fc48725df9e701e32e37c3c
Joeri Hermans <joeri@joerihermans.com>
2016-12-05 16:13:53 +0100

    Set DOWNPOURSocketWorker as default, and set remove EASGD worker

--- distkeras/parameter_servers.py
+++ distkeras/parameter_servers.py
@@ -181,40 +181,7 @@ class SocketParameterServer(ParameterServer):
             t.join()
             del t
 
-class DOWNPOURParameterServer(RESTParameterServer):
-
-    def __init__(self, model, learning_rate, master_port):
-        super(DOWNPOURParameterServer, self).__init__(model, master_port)
-        self.learning_rate = learning_rate
-        self.mutex = Lock()
-
-    def initialize(self):
-
-        ## BEGIN DOWNPOUR REST routes. #########################################
-
-        @self.server.route('/center_variable', methods=['GET'])
-        def center_variable():
-            with self.mutex:
-                center_variable = self.model.get_weights()
-
-            return pickle.dumps(center_variable, -1)
-
-        @self.server.route('/update', methods=['POST'])
-        def update():
-            data = pickle.loads(request.data)
-            variable = data['variable']
-
-            with self.mutex:
-                center_variable = self.model.get_weights()
-                center_variable = center_variable + variable
-                self.model.set_weights(center_variable)
-                self.next_update()
-
-            return 'OK'
-
-        ## END DOWNPOUR REST routes. ###########################################
-
-class DOWNPOURSocketParameterServer(SocketParameterServer):
+class DOWNPOURParameterServer(SocketParameterServer):
 
     def __init__(self, model, learning_rate, master_port):
         super(DOWNPOURSocketParameterServer, self).__init__(model, master_port)
