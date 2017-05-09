4334f56ee3dffb666841e78d17fb61b91cb1c3e5
Natalino Busa <natalino.busa@gmail.com>
2017-05-09 09:44:39 +0200

    Mean Squared Error evaluator for dist-keras
    I have created some examples using dist-keras with regression and AE and I thought this might be a nice to have.

--- distkeras/evaluators.py
+++ distkeras/evaluators.py
@@ -3,6 +3,9 @@
 An evaluator will evaluate a dataframe according to specific requirements.
 """
 
+from pyspark.sql.functions import udf
+from pyspark.mllib.linalg import DenseVector, DoubleType
+
 class Evaluator(object):
     """An evaluator is an abstract class which will, given a label and a prediction,
        will compute an evaluation metric.
@@ -46,3 +49,33 @@ class AccuracyEvaluator(Evaluator):
         validated_instances = cleaned.count()
 
         return float(validated_instances) / float(num_instances)
+
+class MseEvaluator(Evaluator):
+    """Computes the MSE accuracy of the prediction based on the label.
+       It adds a new column on the dataframe, default: mse_score
+
+    # Arguments
+        label_col: string. Label column.
+        prediction_col: string. Prediction column.
+        score_col: string. MSE score column
+    """
+
+    def __init__(self, label_col="label", prediction_col="prediction", score_col="mse_score"):
+        # Initialize the parent structure.
+        super(AccuracyEvaluator, self).__init__(label_col, prediction_col)
+        self.score_col = score_col
+
+    def evaluate(self, dataframe):
+        # spark mse udf function 
+        def mse_func(x,y):
+            return float((DenseVector(x-y).values**2).mean())
+        mse = udf(mse_func, DoubleType())
+        
+        # apply to each predicted row
+        df = df.withColumn(self.score_col, mse(dataframe[self.label_column],dataframe[self.prediction_column])) 
+        
+        # average over all results.
+        mse_score = df.groupBy().avg(self.score_col).collect()[0][0]
+
+        #cast to float and return
+        return float(mse_score)
