0b00c3d2ee6d886891a56feb725f180c9e35afbe
jwang12345 <jwang1@expedia.com>
2017-06-23 16:33:45 -0700

    correct loss log

--- distkeras/workers.py
+++ distkeras/workers.py
@@ -335,7 +335,7 @@ class ADAGWorker(NetworkWorker):
             X, Y = self.get_next_minibatch()
             h = self.model.train_on_batch(X, Y)
             self.add_history(h)
-            sys.stderr.write(h)
+            sys.stderr.write("Iteration: "+ str(self.iteration) +"  loss:"+str(h))
             if self.iteration % self.communication_window == 0:
                 W2 = np.asarray(self.model.get_weights())
                 delta = W2 - W1
